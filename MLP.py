74887be37d1de19f8e8cf55f7094315b19b8b97b
Cassiano Zucco <cassianoczz@users.noreply.github.com>
2019-09-07 02:53:41 -0300

    Codigo 1.2.4

--- MLP.py
+++ MLP.py
@@ -24,7 +24,7 @@ print(y_pred)
 
 with open('Resultados.txt','a') as arquivo:
     arquivo.write(str(DateTime))
-    arquivo.write(str('Accuracy: %.2f' % accuracy_score(Y, y_pred)))
+    arquivo.write(str(' Accuracy: %.2f \n' % accuracy_score(Y, y_pred)))
     arquivo.close()
 
 print('Accuracy: %.2f' % accuracy_score(Y, y_pred))
