a35eafeb6448223341ab4f56e695b44fcff7b0f0
Cassiano Zucco <cassianoczz@users.noreply.github.com>
2019-09-10 05:50:24 -0300

    Tentativa 1.1

--- MLP.py
+++ MLP.py
@@ -9,7 +9,7 @@ DateTime = DTNow.strftime("%d/%m/%Y %H:%M:%S")
 CaminhoTabela = r'~/IAFinancialmarket/BTC_USD_1.2.1.csv'
 tabela = pd.read_csv(CaminhoTabela)
 
-df = pd.DataFrame(tabela, dtype=float64)
+df = pd.DataFrame(tabela, dtype=object)
 print(df)
 
 X = df[['Fechamento','Abertura','Maxima','Minima','Chikou','Tekan','Kinju','Senkou A','Senkou B','Chikou0','Chikou1','Chikou2','T/K 0','T/K 1','T/K 2','Preco X T/K 0','Preco X T/K 1','Preco X T/K 2','Kumo 0','Kumo 1','Kumo 2']]
