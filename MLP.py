c1d219133eb7464a744345706020f721bbd51d8a
cassianoczz <cassio.zucco@hotmail.com>
2019-10-08 08:08:58 -0300

    Arrumando os testes

--- MLP.py
+++ MLP.py
@@ -1,6 +1,7 @@
 from sklearn.neural_network import MLPClassifier
 import pandas as pd
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 from datetime import datetime
 import numpy as np
 # Producao
@@ -14,21 +15,20 @@ tabela = pd.read_csv(CaminhoTabela)
 
 df = pd.DataFrame(tabela)
 
-split  75% teste random
-25% treino random
-
 print(df)
 
 X = df[['Fechamento','Abertura','Maxima','Minima','Chikou','Tekan','Kinju','Senkou A','Senkou B','Chikou0','Chikou1','Chikou2','T/K 0','T/K 1','T/K 2','Preco X T/K 0','Preco X T/K 1','Preco X T/K 2','Kumo 0','Kumo 1','Kumo 2']]
 Y = df['Resultado']
 
+X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.75, random_state=42)
+
 ppn = MLPClassifier()
 
-ppn.fit(X, Y) # treinei
+ppn.fit(X_train, y_train)
 
-X_teste = 
+#X_teste_manual =
 
-y_pred = ppn.predict(X) # predicao
+y_pred = ppn.predict(X_test)
 
 # Producao
 joblib.dump(ppn, 'mlpBTC_USD.pk1')
@@ -48,11 +48,11 @@ with open('Resultados.txt','a') as arquivo:
     arquivo.write('\n')
     arquivo.write(str(y_pred))
     arquivo.write('\n')
-    arquivo.write(str(' Accuracy: %.2f ' % accuracy_score(Y, y_pred)))
+    arquivo.write(str(' Accuracy: %.2f ' % accuracy_score(y_train, y_pred)))
     arquivo.write('\n')
     arquivo.close()
 
-print('Accuracy: %.2f' % accuracy_score(Y, y_pred))
+print('Accuracy: %.2f' % accuracy_score(y_train, y_pred))
 
 
 # escrever na base de dados
