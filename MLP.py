d8a2b0d299e495a312b7f433907d114b05ebe80e
cassianoczz <cassio.zucco@hotmail.com>
2019-11-04 10:02:04 -0300

    Implantacao completo da fuzzy

--- MLP.py
+++ MLP.py
@@ -39,12 +39,12 @@ y_pred = ppn.predict(X_test_manual)
 # joblib.dump(ppn, 'mlpBTC_USD.pk1')
 
 # A Seguir uma imagem com modelo que recebe duas entradas na camada um. 
-# A camada dois, abriga duas funções de pertinência para cada entrada, mapeado para cada conjunto nebuloso. 
-# Esses podem ser otimizados com algoritmos de treinamento ou de otimização. 
-# A terceira camada, possui funções de ativação fixas. 
-# A camada quatro, é responsável pela normalização dos valores processados anteriormente. 
-# A quinta camada, é interligada com entrada para o processamento da função de ativação, que também pode passar por um processo de aprendizagem. 
-# Na última camada, é feito o somatório de todas as saídas anteriores, assim apresentando a saída de ANFIS (Fonseca 2012). 
+# A camada dois, abriga duas funcoes de pertinencia para cada entrada, mapeado para cada conjunto nebuloso. 
+# Esses podem ser otimizados com algoritmos de treinamento ou de otimizacao. 
+# A terceira camada, possui funcoes de ativacao fixas. 
+# A camada quatro, e responsavel pela normalizacao dos valores processados anteriormente. 
+# A quinta camada, e interligada com entrada para o processamento da funcao de ativacao, que tambem pode passar por um processo de aprendizagem. 
+# Na ultima camada, e feito o somatorio de todas as saidas anteriores, assim apresentando a saida de ANFIS (Fonseca 2012). 
 
 
 prediction = pd.DataFrame(y_pred, columns=['predictions']).to_csv('prediction.csv')
@@ -78,75 +78,133 @@ print('Accuracy: %.2f' % accuracy_score(y_test_manual, y_pred))
 # New Antecedent/Consequent objects hold universe variables and membership
 # functions
 
-RNA = ctrl.Antecedent(y_pred, 'RNA')
-PreçoxNuvem = ctrl.Antecedent(np.arange(0, 11, 1), 'PreçoxNuvem')
+#RNA = ctrl.Antecedent(np.arange(0, 3, 1), 'RNA')
+PrecoxNuvem = ctrl.Antecedent(np.arange(0, 11, 1), 'PrecoxNuvem')
 SenkouBfuturaReta = ctrl.Antecedent(np.arange(0, 11, 1), 'SenkouBfuturaReta')
 KumoGrossa = ctrl.Antecedent(np.arange(0, 11, 1), 'KumoGrossa')
-PreçoLongeKinju = ctrl.Antecedent(np.arange(0, 11, 1), 'PreçoLongeKinju')
+PrecoLongeKinju = ctrl.Antecedent(np.arange(0, 11, 1), 'PrecoLongeKinju')
 Pullback = ctrl.Antecedent(np.arange(0, 11, 1), 'Pullback')
+KinjuReta = ctrl.Antecedent(np.arange(0, 11, 1), 'KinjuReta')
 Lateralizacao = ctrl.Antecedent(np.arange(0, 11, 1), 'Lateralizacao')
 
 # Auto-membership function population is possible with .automf(3, 5, or 7)
-PreçoxNuvem.automf(3)
+PrecoxNuvem.automf(3)
 SenkouBfuturaReta.automf(3)
 KumoGrossa.automf(3)
-PreçoLongeKinju.automf(3)
+PrecoLongeKinju.automf(3)
 Pullback.automf(3)
+KinjuReta.automf(3)
 Lateralizacao.automf(3)
 
+#Graphics
+#RNA.view()
+#ANOTACAO: PASSAR OS ARGUMETNOS PARA A FUNCAO
 def Comprado():
-    comprado = ctrl.Consequent(np.arange(0, 100, 1), 'comprado')
 
-    comprado['baixo'] = fuzz.trimf(comprado.universe, [0, 0, 40])
-    comprado['medio'] = fuzz.trimf(comprado.universe, [0, 40, 60])
-    comprado['alto'] = fuzz.trimf(comprado.universe, [40, 60, 100])
+    comprado = ctrl.Consequent(np.arange(0, 101, 1), 'comprado')
 
-    rule1 = ctrl.Rule(RNA['comprado'] & PreçoxNuvem['perto'] & SenkouBfuturaReta['baixo'] & KumoGrossa['pouco'] & PreçoLongeKinju['perto'], comprado['alto'])
-    rule2 = ctrl.Rule(RNA['comprado'] & (PreçoxNuvem['dentro'] | SenkouBfuturaReta['mediano']) & KumoGrossa['mediano'] & (PreçoLongeKinju['mediano'] | Pullback['talvez']) & Lateralizacao['pouco'], comprado['medio'])
-    rule3 = ctrl.Rule(RNA['comprado'] & PreçoxNuvem['longe'] & SenkouBfuturaReta['bastante'] & KumoGrossa['bastante'] & PreçoLongeKinju['perto'] & (Lateralizacao['bastante'] & Pullback['nao']), comprado['alto'])
+    comprado.automf(3)
+
+    # comprado['fraco'] = fuzz.trimf(comprado.universe, [0, 0, 40])
+    # comprado['medio'] = fuzz.trimf(comprado.universe, [0, 40, 60])
+    # comprado['forte'] = fuzz.trimf(comprado.universe, [40, 60, 100])
+
+    regra1 = ctrl.Rule(
+        #RNA['comprado'] & 
+        PrecoxNuvem['average'] & SenkouBfuturaReta['poor'] & KumoGrossa['poor'] & (PrecoLongeKinju['average'] | (KinjuReta['good'] & PrecoLongeKinju['good'] | Pullback['good'])), comprado['good'])
+    regra2 = ctrl.Rule(
+        #RNA['comprado'] & 
+        (PrecoxNuvem['poor'] | SenkouBfuturaReta['average']) & KumoGrossa['average'] & (PrecoLongeKinju['average'] | Pullback['average']) & Lateralizacao['poor'], comprado['average'])
+    regra3 = ctrl.Rule(
+        #RNA['comprado'] & 
+        SenkouBfuturaReta['good'] & KumoGrossa['good'] & Lateralizacao['good'], comprado['poor'])
     
-    return rule1, rule2, rule3
+    #regra1.view()
+
+    comprado_ctrl = ctrl.ControlSystem([regra1, regra2, regra3])
+    comprado_ctrl_Sim = ctrl.ControlSystemSimulation(comprado_ctrl)
+
+    # comprado_ctrl_Sim.input['RNA'] = y_pred
+    comprado_ctrl_Sim.input['PrecoxNuvem'] = 2
+    comprado_ctrl_Sim.input['SenkouBfuturaReta'] = 9
+    comprado_ctrl_Sim.input['KumoGrossa'] = 2
+    comprado_ctrl_Sim.input['PrecoLongeKinju'] = 0
+    comprado_ctrl_Sim.input['Pullback'] = 2
+    comprado_ctrl_Sim.input['KinjuReta'] = 2
+    comprado_ctrl_Sim.input['Lateralizacao'] = 1
+
+    # Crunch the numbers
+    comprado_ctrl_Sim.compute()
+
+    return comprado_ctrl_Sim.output['comprado']
 
 def Aguardar():
     aguardar = ctrl.Consequent(np.arange(0, 100, 1), 'aguardar')
     
-    aguardar['baixo'] = fuzz.trimf(aguardar.universe, [0, 0, 40])
-    aguardar['medio'] = fuzz.trimf(aguardar.universe, [0, 40, 60])
-    aguardar['alto'] = fuzz.trimf(aguardar.universe, [40, 60, 100])
+    aguardar.automf(3)
+
+    #aguardar['fraco'] = fuzz.trimf(aguardar.universe, [0, 0, 40])
+    #aguardar['medio'] = fuzz.trimf(aguardar.universe, [0, 40, 60])
+    #aguardar['forte'] = fuzz.trimf(aguardar.universe, [40, 60, 100])
     
-    rule1 = ctrl.Rule(quality['poor'] | service['poor'], tip['baixo'])
-    rule2 = ctrl.Rule(service['average'], tip['medio'])
-    rule3 = ctrl.Rule(service['good'] | quality['good'], tip['alto'])
+    regra1 = ctrl.Rule(RNA['aguardar'] & SenkouBfuturaReta['good'] & Lateralizacao['good'] & Pullback['poor'] & KinjuReta['good'], aguardar['good'])
+    regra2 = ctrl.Rule(RNA['aguardar'] & PrecoxNuvem['poor'] & SenkouBfuturaReta['average'] & Pullback['poor'] & Lateralizacao['average'], aguardar['average'])
+    regra3 = ctrl.Rule(RNA['aguardar'] & PrecoxNuvem['average'] & SenkouBfuturaReta['poor'] | KinjuReta['poor'] | Lateralizacao['poor'], aguardar['poor'])
+
+    #regra1.view()
+
+    aguardar_ctrl = ctrl.ControlSystem([regra1, regra2, regra3])
+    aguardar_ctrl_Sim = ctrl.ControlSystemSimulation(aguardar_ctrl)
+
+    # Crunch the numbers
+    aguardar_ctrl_Sim.compute()
+
+    return aguardar_ctrl_Sim.output['aguardar']
 
 def Vendido():
     vendido = ctrl.Consequent(np.arange(0, 100, 1), 'vendido')
     
-    vendido['baixo'] = fuzz.trimf(vendido.universe, [0, 0, 40])
-    vendido['medio'] = fuzz.trimf(vendido.universe, [0, 40, 60])
-    vendido['alto'] = fuzz.trimf(vendido.universe, [40, 60, 100])
+    vendido.automf(3)
+
+    # vendido['fraco'] = fuzz.trimf(vendido.universe, [0, 0, 40])
+    # vendido['medio'] = fuzz.trimf(vendido.universe, [0, 40, 60])
+    # vendido['forte'] = fuzz.trimf(vendido.universe, [40, 60, 100])
     
-    rule1 = ctrl.Rule(quality['poor'] | service['poor'], tip['baixo'])
-    rule2 = ctrl.Rule(service['average'], tip['medio'])
-    rule3 = ctrl.Rule(service['good'] | quality['good'], tip['alto'])
+    regra1 = ctrl.Rule(RNA['vendido'] & PrecoxNuvem['average'] & SenkouBfuturaReta['poor'] & KumoGrossa['poor'] & (PrecoLongeKinju['average'] | (KinjuReta['good'] & PrecoLongeKinju['good'] | Pullback['good'])), vendido['good'])
+    regra2 = ctrl.Rule(RNA['vendido'] & (PrecoxNuvem['poor'] | SenkouBfuturaReta['average']) & KumoGrossa['average'] & (PrecoLongeKinju['average'] | Pullback['average']) & Lateralizacao['average'], vendido['average'])
+    regra3 = ctrl.Rule(RNA['vendido'] & SenkouBfuturaReta['good'] & KumoGrossa['good'] & Lateralizacao['good'], vendido['poor'])
+
+    #regra1.view()
+
+    vendido_ctrl = ctrl.ControlSystem([regra1, regra2, regra3])
+    vendido_ctrl_Sim = ctrl.ControlSystemSimulation(vendido_ctrl)
+
+    # Crunch the numbers
+    vendido_ctrl_Sim.compute()
+
+    return vendido_ctrl_Sim.output['vendido']
 
 
-if RNA = comprado 
-fuzzy.comprado()
-return medio fraco
-else fuzzy.aguardar
-return medio fraco
-else fuzzy.vendido
+# if RNA = comprado 
+print (Comprado())
+# return medium fraco
+# else fuzzy.aguardar
+# return medium fraco
+# else fuzzy.vendido
 
-if RNA = aguardar 
-fuzzy.aguardar()
-return medio fraco
-else fuzzy.comprado
-return medio fraco
-else fuzzy.vendido
+# #tip.view(sim=tipping)
+# #comprado().view()
 
-if RNA = vendido 
-fuzzy.vendido()
-return medio fraco
-else fuzzy.comprado
-return medio fraco
-else fuzzy.aguardar
+# if RNA = aguardar 
+# fuzzy.aguardar()
+# return medium fraco
+# else fuzzy.comprado
+# return medium fraco
+# else fuzzy.vendido
+
+# if RNA = vendido 
+# fuzzy.vendido()
+# return medium fraco
+# else fuzzy.comprado
+# return medium fraco
+# else fuzzy.aguardar
